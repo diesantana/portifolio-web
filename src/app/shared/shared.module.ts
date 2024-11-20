b94e6e5fa60547cd756a28e5507ea03b68c31fec
Diego Santana <3c.santana@gmail.com>
2024-11-20 10:01:02 -0300

    feat: Create SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,14 @@ import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
   ]
+
 })
 export class SharedModule { }
