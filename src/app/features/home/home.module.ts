e8575915394ba9493c9bf44a95ab4b5203dc563a
Diego Santana <3c.santana@gmail.com>
2024-12-14 07:45:53 -0300

    feat: Implements main-section

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { MainSectionComponent } from './main-section/main-section.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    MainSectionComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent
   ]
 })
 export class HomeModule { }
